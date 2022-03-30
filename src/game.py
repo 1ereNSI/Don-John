8f02b917e7a1824a7aafa11fa0a07f013da4003e
Ethan <bulinethan@gmail.com>
2022-03-30 20:26:09 +0200

    quatrième commit

--- src/game.py
+++ src/game.py
@@ -13,7 +13,7 @@ class Game:
         pygame.display.set_caption("Don John")
 
         #charger la carte (tmx)
-        tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
+        tmx_data = pytmx.util_pygame.load_pygame('Map/carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
         map_layer.zoom = 2
@@ -22,6 +22,14 @@ class Game:
 
         player_position = tmx_data.get_object_by_name("player")
         self.player = Player(player_position.x, player_position.y)
+
+        #définir une liste qui va stocker les rectangles de collision
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
 
 
         #dessiner le groupe de calques
@@ -34,12 +42,24 @@ class Game:
 
         if pressed[pygame.K_UP]:
             self.player.move_up()
+            self.player.change_animation('up')
         elif pressed[pygame.K_DOWN]:
             self.player.move_down()
+            self.player.change_animation('down')
         elif pressed[pygame.K_RIGHT]:
             self.player.move_right()
+            self.player.change_animation('right')
         elif pressed[pygame.K_LEFT]:
             self.player.move_left()
+            self.player.change_animation('left')
+
+    def update(self):
+        self.group.update()
+
+        #vérification collision
+        for sprite in self.group.sprites():
+            if sprite.feet.collidelist(self.walls) > -1:
+                sprite.move_back()
 
     def run(self):
 
@@ -50,8 +70,9 @@ class Game:
 
         while running:
 
+            self.player.save_location()
             self.handle_input()
-            self.group.update()
+            self.update()
             self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
