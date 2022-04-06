ef12b9bf938cdceca7dd3e0dc44954cbf2b91a55
Ethan <bulinethan@gmail.com>
2022-04-06 22:53:20 +0200

    cinquième commit

--- src/game.py
+++ src/game.py
@@ -1,8 +1,7 @@
 import pygame
-import pytmx
-import pyscroll
 
 from player import Player
+from src.map import MapManager
 
 
 class Game:
@@ -12,29 +11,10 @@ class Game:
         self.screen = pygame.display.set_mode((1080, 720))
         pygame.display.set_caption("Don John")
 
-        #charger la carte (tmx)
-        tmx_data = pytmx.util_pygame.load_pygame('Map/carte.tmx')
-        map_data = pyscroll.data.TiledMapData(tmx_data)
-        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        map_layer.zoom = 2
-
         #generer joueur
 
-        player_position = tmx_data.get_object_by_name("player")
-        self.player = Player(player_position.x, player_position.y)
-
-        #définir une liste qui va stocker les rectangles de collision
-        self.walls = []
-
-        for obj in tmx_data.objects:
-            if obj.type == "collision":
-                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
-
-
-
-        #dessiner le groupe de calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
-        self.group.add(self.player)
+        self.player = Player(0, 0)
+        self.map_manager = MapManager(self.screen, self.player)
 
     def handle_input(self):
 
@@ -54,12 +34,7 @@ class Game:
             self.player.change_animation('left')
 
     def update(self):
-        self.group.update()
-
-        #vérification collision
-        for sprite in self.group.sprites():
-            if sprite.feet.collidelist(self.walls) > -1:
-                sprite.move_back()
+        self.map_manager.update()
 
     def run(self):
 
@@ -73,8 +48,7 @@ class Game:
             self.player.save_location()
             self.handle_input()
             self.update()
-            self.group.center(self.player.rect.center)
-            self.group.draw(self.screen)
+            self.map_manager.draw()
             pygame.display.flip()
 
             for event in pygame.event.get():
