f24d87d8b5070c40ee40199294316a2b5fbc9989
Ethan <bulinethan@gmail.com>
2022-05-25 16:47:32 +0200

    troisième commit

--- src/animation.py
+++ src/animation.py
@@ -2,20 +2,26 @@ import pygame
 
 class AnimateSprite(pygame.sprite.Sprite):
 
-    def __init__(self, sprite_name, speed):
+    def __init__(self, sprite_name, speed, len_images=3, len_image=32):
         super().__init__()
         self.sprite_sheet = pygame.image.load(f'../sprites/{sprite_name}.png')
         self.animation_index = 0
         self.clock = 0
+        self.len_images = len_images
+        self.len_image = len_image
         self.images = {
             'down': self.get_images(0),
             'left': self.get_images(32),
             'right': self.get_images(64),
             'up': self.get_images(96)
         }
+
         self.speed_change = speed
 
     def change_animation(self, name):
+        """ Change le sprite d'une entité donnée en parcourant sa spritesheet
+            :return: None
+            """
         self.image = self.images[name][self.animation_index]
         self.image.set_colorkey(self.image.get_at((0, 0)))
         self.clock += self.speed_change * 5
@@ -30,16 +36,22 @@ class AnimateSprite(pygame.sprite.Sprite):
             self.clock = 0
 
     def get_images(self, y):
+        """ Récupère chaque image à une ordonnée donnée et les ajoute à une liste images
+            :return: images
+            """
         images = []
 
-        for i in range(0, 3):
-            x = i*32
+        for i in range(0, self.len_images):
+            x = i * self.len_image
             image = self.get_image(x, y)
             images.append(image)
 
         return images
 
     def get_image(self, x, y):
-        image = pygame.Surface([32, 32])
+        """ Récupère une image de surface 32x32 aux coordonnées données
+            :return: image
+            """
+        image = pygame.Surface([self.len_image, self.len_image])
         image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
         return image
--- src/entity.py
+++ src/entity.py
@@ -4,52 +4,74 @@ from src.animation import AnimateSprite
 
 class Entity(AnimateSprite):
 
-    def __init__(self, name, sprite_name, x, y, speed):
-        super().__init__(sprite_name, speed)
+    def __init__(self, name, sprite_name, x, y, speed, len_images=3, len_image=32):
+        super().__init__(sprite_name, speed, len_images, len_image)
         self.image = self.get_image(0, 0)
-        self.image.set_colorkey(self.image.get_at((0,0)))
+        self.image.set_colorkey(self.image.get_at((0, 0)))
         self.rect = self.image.get_rect()
         self.position = [x, y]
         self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)
+        self.rect_dialogue = pygame.Rect(0, 0, self.rect.width+10, self.rect.height+10)
         self.old_position = self.position.copy()
         self.speed_entity = speed
+        self.object_speed = 0
 
+    #enregistre les coordonées actuelles du joueur
     def save_location(self): self.old_position = self.position.copy()
 
+    # mouvements des entités et accélérations
     def move_right(self):
         self.change_animation("right")
         self.position[0] += self.speed_entity
+
+    def move_right_speed(self):
+        self.change_animation("right")
+        self.position[0] += (self.speed_entity + self.object_speed)
 
     def move_left(self):
         self.change_animation("left")
         self.position[0] -= self.speed_entity
 
+    def move_left_speed(self):
+        self.change_animation("left")
+        self.position[0] -= (self.speed_entity + self.object_speed)
+
     def move_up(self):
         self.change_animation("up")
         self.position[1] -= self.speed_entity
+
+    def move_up_speed(self):
+        self.change_animation("up")
+        self.position[1] -= (self.speed_entity + self.object_speed)
 
     def move_down(self):
         self.change_animation("down")
         self.position[1] += self.speed_entity
 
+    def move_down_speed(self):
+        self.change_animation("down")
+        self.position[1] += (self.speed_entity + self.object_speed)
+
     def update(self):
         self.rect.topleft = self.position
         self.feet.midbottom = self.rect.midbottom
+        self.rect_dialogue.midbottom = self.rect.midbottom
 
     def move_back(self):
         self.position = self.old_position
         self.rect.topleft = self.position
         self.feet.midbottom = self.rect.midbottom
+        self.rect_dialogue.midbottom = self.rect.midbottom
     
 class Player(Entity):
     
     def __init__(self):
-        super().__init__("player", "ange", 0, 0, 1)
+        super().__init__("player", "player2", 0, 0, 2)
 
 class NPC(Entity):
 
-    def __init__(self, name, sprite_name, nb_points, dialog=[]):
-        super().__init__(name, sprite_name, 1, 0, 0)
+    def __init__(self, name, sprite_name, speed=0, nb_points=1, dialog=[], len_images=3, len_image=32):
+        super().__init__(name, sprite_name, 0, 0, speed, len_images, len_image)
         self.nb_points = nb_points
         self.points = []
         self.dialog = dialog
@@ -57,8 +79,11 @@ class NPC(Entity):
         self.current_point = 0
 
     def move(self):
+        """ Permet aux npcs, aux runes et aux anges de se déplacer selon un chemin définit
+            :return: None
+            """
         current_point = self.current_point
-        target_point = self.current_point + self.speed
+        target_point = self.current_point + self.speed_entity
         
         if target_point >= self.nb_points:
             target_point = 0
@@ -85,14 +110,22 @@ class NPC(Entity):
         self.save_location()
 
     def load_points(self, tmx_data):
+        """ Cherche le chemin de l'entité sur la map et ajoute le rect de chaque point de passage à une liste
+            :return: None
+            """
         for num in range(1, self.nb_points+1):
             point = tmx_data.get_object_by_name(f"{self.name}_path{num}")
             rect = pygame.Rect(point.x, point.y, point.width, point.height)
             self.points.append(rect)
 
 class Ange(NPC):
-    def __init__(self, name, sprite_name, nb_points):
-        super().__init__(name, sprite_name, nb_points)
+    def __init__(self, name, sprite_name, speed, nb_points):
+        super().__init__(name, sprite_name, speed, nb_points)
 
+class Rune(NPC):
+    def __init__(self, name):
+        self.name = name
+        self.dialogue = [f"bravo tu as obtenu la {self.name}"]
+        super().__init__(name, "objet", 1, 1, dialog=self.dialogue, len_images=4, len_image=16)
 
 
